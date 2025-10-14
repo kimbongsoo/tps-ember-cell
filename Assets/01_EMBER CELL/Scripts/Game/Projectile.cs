1df265b7749cfa7e5e1107fbb5b0359280600400
kimbongsoo <kbsu1881@naver.com>
2025-10-15 05:02:27 +0900

    fix : Projectile구조개선(UnityEngine.pool) #1

--- Assets/01_EMBER CELL/Scripts/Game/Projectile.cs
+++ Assets/01_EMBER CELL/Scripts/Game/Projectile.cs
@@ -1,77 +1,81 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-
+using UnityEngine.Pool;
 namespace TEC
 {
     public class Projectile : MonoBehaviour
     {
-        public float speed = 100f;
-        public float lifeTime = 3f;
-        public static string[] materialNames = { "Wood", "Rock", "Dirt", "Metal" };
+        private IObjectPool<Projectile> pool;
+        private float lifetime = 3f;
+        private float lifeTimer;
 
-        private float damage = 0f;
+        private Rigidbody rb;
+        private bool initialized = false;
+
         private GameObject attacker;
- 
+        private float damage;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
         public void Initialize(GameObject attacker, float damage)
         {
             this.attacker = attacker;
             this.damage = damage;
+            lifeTimer = lifetime;
+            initialized = true;
+
+            rb.velocity = transform.forward * 100f;
         }
 
-        private void Start()
+        public void SetPool(IObjectPool<Projectile> pool)
         {
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * speed, ForceMode.Impulse);
+            this.pool = pool;
+        }
 
-            Destroy(gameObject, lifeTime);
+        private void Update()
+        {
+            if (!initialized) return;
+
+            lifeTimer -= Time.deltaTime;
+            if (lifeTimer <= 0f)
+            {
+                ReleaseToPool();
+            }
         }
 
         private void OnCollisionEnter(Collision collision)
         {
-
-            // Debug.Log(collision.gameObject.name);
-            //총알 충돌처리에 대한 구현
-            //TODO 1. Effect를 출력.
-            //TODO 2. Damage 처리하기 -> 캐릭터, 배경에 맞았는지
-            if (attacker && collision.transform.IsChildOf(attacker.transform))
+            if (collision.transform.gameObject == attacker)
                 return;
 
-            // 임팩트 이펙트 -> 이펙트매니저로 책임전가
-            // string material = null;
-            // var physMat = collision.collider ? collision.collider.sharedMaterial : null;
-            // if (physMat != null)
-            //     material = materialNames.FirstOrDefault(type => physMat.name.Contains(type));
-
-            // if (EffectManager.Instance.GetEffect(material, out GameObject effect))
-            // {
-            //     var contact = collision.contacts[0];
-            //     effect.transform.position = contact.point + contact.normal * 0.01f;
-            //     effect.transform.rotation = Quaternion.LookRotation(contact.normal);
-            //     Destroy(effect, 2f);
-            // }
-
-            //데미지 전달
-            // collision.transform.root.GetComponent<IDamageReceiver>();
-            var receiver = collision.collider.GetComponentInParent<IDamageReceiver>();
-            if (receiver != null)
+            if (collision.transform.TryGetComponent(out IDamageReceiver receiver))
             {
                 var data = new DamageData(damage, attacker);
                 receiver.ReceiveDamage(data);
             }
 
-            string material = materialNames.FirstOrDefault(type => collision.collider.material.name.Contains(type));
-
-            if (EffectManager.Instance.GetEffect(material, out GameObject effect))
-            {
-                effect.transform.position = collision.contacts[0].point;
-                effect.transform.forward = collision.contacts[0].normal;
-            }
-            Destroy(gameObject);
+            ReleaseToPool();
         }
 
-    }
+        private void ReleaseToPool()
+        {
+            if (pool != null)
+            {
+                pool.Release(this); 
+            }
+            else
+            {
+                gameObject.SetActive(false); 
+            }
+        }
 
+
+        private void OnDisable()
+        {
+            rb.velocity = Vector3.zero;
+            initialized = false;
+        }
+    }
 }
