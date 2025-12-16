12bfc72985653556052c821c6e370c9f69356c05
kimbongsoo <kbsu1881@naver.com>
2025-12-16 22:36:07 +0900

    feat : InventoryUI script 추가

--- Assets/01_EMBER CELL/Scripts/Game/Interaction System/InventoryUI.cs
+++ Assets/01_EMBER CELL/Scripts/Game/Interaction System/InventoryUI.cs
@@ -6,16 +6,6 @@ namespace TEC
 {
     public class InventoryUI : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
