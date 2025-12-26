6b68c77643eea5cda1021cefd4ccca8d3cb45e49
kimbongsoo <kbsu1881@naver.com>
2025-12-26 22:56:20 +0900

    remove : InventoryKey, PlayerInventory  제거

--- Assets/01_EMBER CELL/Scripts/Game/Interaction System/InteractionDropItem.cs
+++ Assets/01_EMBER CELL/Scripts/Game/Interaction System/InteractionDropItem.cs
@@ -21,41 +21,26 @@ namespace TEC
             int index = Mathf.Clamp(itemData.ItemGrade - 1, 0, itemGradeMaterials.Length - 1);
             visualRenderer.material = itemGradeMaterials[index];
         }
-        //원본
-        // public void Interact(IInteractionData data)
-        // {
-        //     //TODO : 아이템 획득 처리
-        //     //TODO : 인벤토리에 추가
-        //     if (data is InteractionDropItemData itemData)
-        //     {
-        //         var playerController = CharacterPlayerController.Instance;
-        //         if (playerController != null)
-        //         {
-        //             var character = playerController.GetComponent<CharacterBase>();
-        //             if (character != null && character.PrimaryWeapon != null)
-        //             {
-        //                 character.PrimaryWeapon.AddAmmo(itemData.AmmoAmount, out int current, out int max);
-        //                 MainHUD.Instance.SetAmmoText(current, max);
-        //             }
-        //         }
-        //     }
-
-        //     Destroy(gameObject);
-        // }
         public void Interact(IInteractionData data)
         {
+            //TODO : 아이템 획득 처리
+            //TODO : 인벤토리에 추가
             if (data is InteractionDropItemData itemData)
             {
-                if (PlayerInventory.Instance != null)
+                var playerController = CharacterPlayerController.Instance;
+                if (playerController != null)
                 {
-                    bool added = PlayerInventory.Instance.TryAddItem(itemData, 1);
-                    if (added)
+                    var character = playerController.GetComponent<CharacterBase>();
+                    if (character != null && character.PrimaryWeapon != null)
                     {
-                        Destroy(gameObject);
-                        return;
+                        character.PrimaryWeapon.AddAmmo(itemData.AmmoAmount, out int current, out int max);
+                        MainHUD.Instance.SetAmmoText(current, max);
                     }
                 }
             }
+
+            Destroy(gameObject);
         }
+
     }
 }
