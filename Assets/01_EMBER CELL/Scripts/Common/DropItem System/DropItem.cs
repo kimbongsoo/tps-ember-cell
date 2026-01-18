00a3282dc17008b36884f47bf1ff4667c682e0f1
kimbongsoo <kbsu1881@naver.com>
2026-01-19 01:08:58 +0900

    feat : DropItem 추가

--- Assets/01_EMBER CELL/Scripts/Common/DropItem System/DropItem.cs
+++ Assets/01_EMBER CELL/Scripts/Common/DropItem System/DropItem.cs
@@ -6,6 +6,7 @@ namespace TEC
     {
         [SerializeField] private ItemDataSO itemDataSO;
         [SerializeField] private int quantity = 1;
+        [SerializeField] private MeshRenderer visualRenderer;
 
         public ItemDataSO ItemDataSO => itemDataSO;
         public string ItemID => itemDataSO != null ? itemDataSO.ItemID : string.Empty;
