4125569c9ef5ad6a275ef95ae78b8fd2bf549944
kimbongsoo <kbsu1881@naver.com>
2026-02-06 03:30:45 +0900

    feat : CharacterBase기반 AIController Move 추가

--- Assets/01_EMBER CELL/Scripts/Game/Character/AI System/AIController.cs
+++ Assets/01_EMBER CELL/Scripts/Game/Character/AI System/AIController.cs
@@ -8,26 +8,50 @@ namespace TEC
     public class AIController : MonoBehaviour
     {
         //dummy transform
-        [SerializeField] private Transform dummyDestination;
+        [SerializeField] private Transform destinationPoint;
+        private CharacterBase characterBase;
         private NavMeshAgent navAgent;
         private AISensor sensor;
         private AIBrain brain;
 
         private void Awake()
         {
+            characterBase = GetComponent<CharacterBase>();
             navAgent = GetComponent<NavMeshAgent>();
             sensor = GetComponentInChildren<AISensor>();
             brain = GetComponent<AIBrain>();
+
+            navAgent.updatePosition = false;
+            navAgent.updateRotation = false;
+        }
+
+        public void Start()
+        {
+            navAgent.speed = characterBase.moveSpeed;
         }
 
         public void Update()
         {
-            SetDestination(dummyDestination.position);
+            //TODO : 목적지 설정 코드를 Update -> AI가 플레이어 위치 감지했을 때 위치로 설정
+            navAgent.SetDestination(destinationPoint.position); //목적지 설정 
+            navAgent.nextPosition = transform.position; // NavMeshAgent의 위치를 캐릭터의 위치로
+
+            if (navAgent.hasPath)
+            {
+                Vector3 moveDirection = (navAgent.steeringTarget - transform.position).normalized; //목적지 방향 계산
+                Vector2 input = new Vector2(moveDirection.x, moveDirection.z); // 2D 입력벡터 생성
+
+                characterBase.Move(input, 0); //이동
+            }
+            else
+            {
+                characterBase.Move(Vector2.zero, 0);
+            }
         }
 
-        public void SetDestination(Vector3 destination)
-        {
-            navAgent.SetDestination(destination);
-        }
+        // public void SetDestination(Vector3 destination)
+        // {
+        //     navAgent.SetDestination(destination);
+        // }
     }
 }
