4d6a275e67cc8248217dc11c7c010a9df2cdbcdd
kimbongsoo <kbsu1881@naver.com>
2026-03-03 05:38:47 +0900

    fix : CombatState 조준 부위 랜덤하게 변경

--- Assets/01_EMBER CELL/Scripts/Game/Character/AI System/CombatState.cs
+++ Assets/01_EMBER CELL/Scripts/Game/Character/AI System/CombatState.cs
@@ -35,10 +35,44 @@ namespace TEC
             direction.y = 0; //y축 회전 방지
             brain.transform.forward = direction;
 
-            Transform targetPoint = brain.TargetCharacter.GetAvatarBoneTransform(HumanBodyBones.Spine);
+            // Transform targetPoint = brain.TargetCharacter.GetAvatarBoneTransform(HumanBodyBones.Spine);
+            Transform targetPoint = GetRandomAimBoneTransform(brain.TargetCharacter);
+            if (targetPoint == null)
+                return;
 
             brain.AIController.SetAiming(targetPoint.position);
             brain.AIController.Fire();
+        }
+
+        private Transform GetRandomAimBoneTransform(CharacterBase targetCharacter)
+        {
+            float weight = Random.Range(0f, 1f);
+
+            if (weight <= 1f / 6f)
+            {
+                return targetCharacter.GetAvatarBoneTransform(HumanBodyBones.Head);
+            }
+
+            if (weight <= 2f / 6f)
+            {
+                return targetCharacter.GetAvatarBoneTransform(HumanBodyBones.Chest);
+            }
+
+            if (weight <= 3f / 6f)
+            {
+                return targetCharacter.GetAvatarBoneTransform(HumanBodyBones.RightUpperArm);
+            }
+
+            if (weight <= 4f / 6f)
+            {
+                return targetCharacter.GetAvatarBoneTransform(HumanBodyBones.LeftUpperArm);
+            }
+
+            if (weight <= 5f / 6f)
+            {
+                return targetCharacter.GetAvatarBoneTransform(HumanBodyBones.RightUpperLeg);
+            }
+            return targetCharacter.GetAvatarBoneTransform(HumanBodyBones.LeftUpperLeg);
         }
     }
 }
